c20ec928a4aa69203128ba03f82f479c81c16786
Luiz Eduardo <l.eduardo8316@gmail.com>
2023-02-23 19:58:50 -0300

    feat: add initial setup

---
+++ src/infra/database.ts
@@ -0,0 +1,17 @@
+import { Knex, knex as setupKnex } from 'knex'
+
+const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+const knex = setupKnex(config)
+
+export { knex, config }
